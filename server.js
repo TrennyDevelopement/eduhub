98eea19818f006b954635e159d9cb43381f93f4b
Trenny Development <asiasartofficial@gmail.com>
2025-12-08 21:40:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Store logs in memory (in production, you might want to use a logging service)
 const logs = [];
